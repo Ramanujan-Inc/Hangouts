e835e7e77c0833ee0551081d60353dd1d7b6d501
Reymel Sardenia <sardeniareymel@gmail.com>
2026-08-23 13:01:54 +0800

    chore: update mock profile avatars

--- web/src/data/mock.ts
+++ web/src/data/mock.ts
@@ -16,10 +16,14 @@ export const memberList: Member[] = Object.values(members)
 export const selectableMembers: Member[] = [members.jam, members.dave, members.chloe]
 
 export const profileAvatars = [
+  'https://api.dicebear.com/7.x/adventurer/svg?hair=long05&hairColor=e5d7a3&skinColor=f2d3b1&eyes=variant01&eyebrows=variant02&mouth=variant12&features=blush&featuresProbability=100&earrings=variant01&earringsProbability=100',
+  'https://api.dicebear.com/7.x/adventurer/svg?seed=Jack',
   'https://api.dicebear.com/7.x/adventurer/svg?seed=mika',
-  'https://api.dicebear.com/7.x/adventurer/svg?seed=bunny',
-  'https://api.dicebear.com/7.x/adventurer/svg?seed=adventurer-1',
-  'https://api.dicebear.com/7.x/adventurer/svg?seed=happy-cat',
+  'https://api.dicebear.com/7.x/adventurer/svg?seed=Aria',
+  'https://api.dicebear.com/7.x/adventurer/svg?seed=Oliver',
+  'https://api.dicebear.com/7.x/adventurer/svg?hair=short16&hairColor=afafaf&skinColor=f2d3b1&eyes=variant10&eyebrows=variant08&mouth=variant24&earrings=variant02&earringsProbability=100',
+  'https://api.dicebear.com/7.x/adventurer/svg?hair=long10&hairColor=cb6820&skinColor=f2d3b1&eyes=variant08&eyebrows=variant02&mouth=variant12&features=blush&featuresProbability=100&earrings=variant03&earringsProbability=100',
+  'https://api.dicebear.com/7.x/adventurer/svg?hair=long21&hairColor=592454&skinColor=f2d3b1&eyes=variant22&eyebrows=variant10&mouth=variant05&features=blush&featuresProbability=100&glasses=variant05&glassesProbability=100',
 ]
 
 export type NoteType = 'butter' | 'blush' | 'sea'
