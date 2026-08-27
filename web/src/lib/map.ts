64aeda2549494028143ede759b519ad4de8bb314
Reymel Sardenia <sardeniareymel@gmail.com>
2026-08-27 14:53:41 +0800

    hotfix: update CARTO tile URL query parameter from api_key to key

--- web/src/lib/map.ts
+++ web/src/lib/map.ts
@@ -4,5 +4,5 @@ export const CARTO_ATTRIBUTION =
   '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>';
 
 export const CARTO_VOYAGER_TILE_URL = CARTO_API_KEY
-  ? `https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png?api_key=${CARTO_API_KEY}`
+  ? `https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png?key=${CARTO_API_KEY}`
   : 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png';
